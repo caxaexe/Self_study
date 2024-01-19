0b95332ad2667d47ba46249ca21a9e1476e7a13a
caxaexe <fasvenomo@gmail.com>
2024-01-19 21:45:51 +0200

    C++

---
+++ C++/first_2.cpp
@@ -0,0 +1,24 @@
+//Запрограммировать следующее выражение: (а + b — f / а) + f * a * a — (a + b) Числа а, b, f вводятся с клавиатуры. Организовать пользовательский интерфейс, таким образом, чтобы было понятно, в каком порядке должны вводиться числа.//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+
+    int a, b, f, x;
+
+    cout << "Введите цифры a, b, f : " << endl;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+    cout << "f = ";
+    cin >> f;
+
+    cout << "Вычисляем по формуле: x = (a + b - f / a) + f * a * a - (a + b)" << endl;
+    x = (a + b - f / a) + f * a * a - (a + b);
+    cout << "x = " << x;
+
+    return 0;
+}
---
+++ C++/first_3.cpp
@@ -0,0 +1,16 @@
+//Напишите программу, которая позволяет пользователю ввести в консоли латинскую букву нижнего регистра, переводит её в верхний регистр и результат выводит в консоль.//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+
+    char letter, upperletter;
+    cout << "Введите букву нижнего регистра :" << endl;
+    cin >> letter;
+    upperletter = letter - 32;
+    cout << "Та же буква в верхнем регистре : " << upperletter;
+
+    return 0;
+}
