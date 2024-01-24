8ab3c0326063891993155f9678fc0c6a9f41e12b
caxaexe <fasvenomo@gmail.com>
2024-01-24 12:59:14 +0200

    C++

---
+++ C++/first_5.cpp
@@ -0,0 +1,25 @@
+//—оставьте программу, котора€ напечатает рисунок, использу€ символы из таблицы ASCII (по соотв. рисунку ????????//
+//                       ????????).//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << char(201);
+    cout << char(203);
+    cout << char(203);
+    cout << char(203);
+    cout << char(203);
+    cout << char(203);
+    cout << char(203);
+    cout << char(187) << endl;
+    cout << char(200);
+    cout << char(202);
+    cout << char(202);
+    cout << char(202);
+    cout << char(202);
+    cout << char(202);
+    cout << char(202);
+    cout << char(188);
+
+}
